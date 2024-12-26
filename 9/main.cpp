b83757f46ac7b8d4df783d81da2a7738485b77c6
tajimyradow <tajimyradow12345@gmail.com>
2024-12-26 19:14:42 +0500

    [solved]: 9

--- 9/main.cpp
+++ 9/main.cpp
@@ -1,5 +1,24 @@
 #include<iostream>
-using namespace std;
-int main(){
 
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0)) {
+            return false;
+        }
+
+        int reverse = 0;
+        while (x > reverse) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+        return (x == reverse) || (x == reverse / 10);
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.isPalindrome(12321);
 }
