02a92f592e4ea7fbbaf6b7c12a7f69b222ff79d2
tajimyradow <tajimyradow12345@gmail.com>
2025-06-24 00:51:49 +0500

    [solved]: 206

---
+++ 206/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+        {
+            return head;
+        }
+        ListNode *newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+
+        return newHead;
+    }
+};
+
+/*
+
+head [1,2]
+[2]
+
+*/
+
+int main()
+{
+
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    head->next->next->next->next->next = new ListNode(6);
+
+    Solution s;
+
+    head = s.reverseList(head);
+}
