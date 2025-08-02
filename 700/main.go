b0abba03682246f151198c42dbf357ff8c8b8d37
tajimyradow <tajimyradow12345@gmail.com>
2025-08-02 18:19:09 +0500

    [solved]: 700

---
+++ 700/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == val {
+		return root
+	}
+
+	if root.Val > val {
+		return searchBST(root.Left, val)
+	}
+
+	return searchBST(root.Right, val)
+}
+
+func main() {
+	root := &TreeNode{
+		Val: 4,
+		Left: &TreeNode{
+			Val: 2,
+		},
+	}
+
+	fmt.Println(searchBST(root, 2))
+}
